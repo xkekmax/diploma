3613784163d9a1c36f22a6605b21957aa2707d61
xkekmax <janeeverlasting66613@gmail.com>
2024-11-17 02:01:44 +0300

    feat: finished BookPage

--- vue-bookstore/src/components/HomePage.vue
+++ vue-bookstore/src/components/HomePage.vue
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import Header from './Header.vue'
 import BookList from './BookList.vue'
-import Drawer from './Drawer.vue'
+// import Drawer from './Drawer.vue'
 
 const items = ref([]);
 
@@ -49,24 +49,27 @@ watch(filters, fetchItems);
 
 </script>
 <template>
+
   <div class="bg-white w-4/5 m-auto rounded-2xl shadow-xl mt-14">
 
-<Header :onChangeSearchInput="onChangeSearchInput"/>
+  <!-- <Drawer/> -->
 
-<div class="p-14">
-  <div class="flex justify-between items-center mb-8">
-    <h2 class="text-3xl font-bold text-orange-900">Все книги</h2>
-    <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
-      <option value=1>Проза</option>
-      <option value=2>Фантастика</option>
-      <option value=3>Фэнтези</option>
-    </select>
+  <Header :onChangeSearchInput="onChangeSearchInput"/>
+
+  <div class="p-14">
+    <div class="flex justify-between items-center mb-8">
+      <h2 class="text-3xl font-bold text-orange-900">Все книги</h2>
+      <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+        <option value=1>Проза</option>
+        <option value=2>Фантастика</option>
+        <option value=3>Фэнтези</option>
+      </select>
+    </div>
+
+    <div class="mt-12">
+      <BookList :items="items"/>
+    </div>
+
   </div>
-
-  <div class="mt-12">
-    <BookList :items="items"/>
-  </div>
-
-</div>
 </div>
 </template>
