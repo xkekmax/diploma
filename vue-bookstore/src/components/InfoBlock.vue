40a0376bd2db826a7c98a6c1b313977f53fe5115
xkekmax <janeeverlasting66613@gmail.com>
2024-12-09 18:23:07 +0300

    drawer_page

---
+++ vue-bookstore/src/components/InfoBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: String,
+  description: String,
+  imageUrl: String
+})
+</script>
+
+<template>
+ <div class="flex flex-col items-center text-center w-80 mx-auto">
+  <img :src="imageUrl" height="90" width="90" alt="Info image"/>
+  <h2 class="mt-4 text-2xl font-medium">{{ title }}</h2>
+  <p class="text-gray-400 mt-2"> {{ description }}</p>
+ </div>
+</template>
