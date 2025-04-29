4cd7d74da2ea2bd16c813441986b8f16dbbe99d0
xkekmax <janeeverlasting66613@gmail.com>
2025-04-29 12:57:02 +0300

    added user's menu

--- vue-bookstore/src/components/Header.vue
+++ vue-bookstore/src/components/Header.vue
@@ -1,12 +1,39 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { inject } from 'vue';
+import { inject, ref } from 'vue';
 
 defineProps({ price: Number });
 const emit = defineEmits(['openDrawer']);
 const isAuthorized = inject('isAuthorized');
 const userName = inject('userName');
+const setUserAuthorized = inject('setUserAuthorized');
+const setUserName = inject('setUserName');
 
+const showDropdown = ref(false);
+let hideTimeout;
+
+const onMouseEnter = () => {
+  clearTimeout(hideTimeout);
+  showDropdown.value = true;
+};
+
+const onMouseLeave = () => {
+  hideTimeout = setTimeout(() => {
+    showDropdown.value = false;
+  }, 300); // задержка в 300 мс
+};
+
+const logout = () => {
+  localStorage.removeItem('user_id');
+  localStorage.removeItem('user_name');
+  setUserAuthorized(false);
+  setUserName('');
+  window.location.reload(); // Перезагрузка страницы
+};
+
+const editProfile = () => {
+  alert('Редактирование профиля пока не реализовано');
+};
 </script>
 
 <template>
@@ -35,12 +62,26 @@ const userName = inject('userName');
         </li>
       </router-link>
 
-      <router-link v-if="isAuthorized" to="/profile">
-        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-red-400">
+      <li
+        v-if="isAuthorized"
+        class="relative flex items-center cursor-pointer gap-3 text-gray-500 hover:text-red-400"
+        @mouseenter="onMouseEnter"
+        @mouseleave="onMouseLeave"
+      >
+        <div class="flex items-center gap-3">
           <img src="/cat.svg" alt="Profile" />
-          <span>{{ userName }}</span> <!-- Отображаем имя пользователя -->
-        </li>
-      </router-link>
+          <span>{{ userName }}</span>
+        </div>
+
+        <ul
+          v-show="showDropdown"
+          class="absolute top-full right-0 mt-2 bg-white border border-gray-200 shadow-lg rounded-md z-10 w-40 transition-opacity duration-200"
+        >
+          <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer" @click="$router.push('/profile')">Заказы</li>
+          <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer" @click="editProfile">Редактировать</li>
+          <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer text-red-500" @click="logout">Выйти</li>
+        </ul>
+      </li>
 
       <router-link v-else to="/autorization">
         <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-red-400">
