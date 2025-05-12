a5a50355d5c5bd3398a5a983d796ff3a432d362e
xkekmax <janeeverlasting66613@gmail.com>
2025-05-12 17:04:43 +0300

    fixed page's size

--- vue-bookstore/src/components/Footer.vue
+++ vue-bookstore/src/components/Footer.vue
@@ -1,15 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="flex justify-around px-16 py-12 bg-orange-300 text-white">
+  <div class="flex justify-between px-20 pt-20 pb-14 bg-orange-300 text-white text-sm">
     <div>
       <ul>
-        <li class="text-2xl mb-2">+7 812 601-0-601</li>
+        <li class="text-xl mb-2">+7 812 601-0-601</li>
         <li>Круглосуточно</li>
         <div class="flex items-center gap-5 mt-6">
-          <a href="https://www.youtube.com" target="_blank"><img class="h-8" src="/icon-youtube.png" alt="YouTube"></a>
-          <a href="https://vk.com" target="_blank"><img class="h-8" src="/icon-vk.png" alt="VK"></a>
-          <a href="https://web.telegram.org" target="_blank"><img class="h-8" src="/icon-telegram.png" alt="Telegram"></a>
-          <a href="https://twitter.com" target="_blank"><img class="h-8" src="/icon-twitter.png" alt="Twitter"></a>
+          <a href="https://www.youtube.com" target="_blank"><img class="h-6" src="/icon-youtube.png" alt="YouTube"></a>
+          <a href="https://vk.com" target="_blank"><img class="h-6" src="/icon-vk.png" alt="VK"></a>
+          <a href="https://web.telegram.org" target="_blank"><img class="h-6" src="/icon-telegram.png" alt="Telegram"></a>
+          <a href="https://twitter.com" target="_blank"><img class="h-6" src="/icon-twitter.png" alt="Twitter"></a>
         </div>
       </ul>
     </div>
